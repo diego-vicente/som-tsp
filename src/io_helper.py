ea785be73d8fee0b92f41888b9dfa40a25907a47
Diego Vicente <diegovicente@protonmail.com>
2017-11-16 18:48:12 +0100

    Use pandas to read .tsp files

---
+++ src/io_helper.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+def read_tsp(filename):
+    with open(filename) as f:
+        node_coord_start = None
+        dimension = None
+        lines = f.readlines()
+
+        # Obtain the information about the .tsp
+        i = 0
+        while not dimension or not node_coord_start:
+            line = lines[i]
+            if line.startswith('DIMENSION :'):
+                dimension = int(line.split()[-1])
+            if line.startswith('NODE_COORD_SECTION'):
+                node_coord_start = i
+            i = i+1
+
+        print('Problem with {} cities read.'.format(dimension))
+
+        f.seek(0)
+
+        # Read a data frame out of the file descriptor
+        cities = pd.read_csv(
+            f,
+            skiprows=node_coord_start + 1,
+            sep=' ',
+            names=['city', 'x', 'y'],
+            dtype={'city': str, 'x': np.float64, 'y': np.float64},
+            header=None,
+            nrows=dimension
+        )
+
+        return cities
