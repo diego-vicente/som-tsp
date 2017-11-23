1a2518fa3b7b5b7c24c2fc46f41fc9c8bf6efc5f
Diego Vicente <diegovicente@protonmail.com>
2017-11-23 22:33:51 +0100

    Neighborhood generation added

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ from neuron import generate_network
 def main():
     problem = read_tsp('assets/qa194.tsp')
 
-    route = som(problem[:10], 10)
+    route = som(problem[:10], 100)
 
     return
 
-def som(problem, iterations):
+def som(problem, iterations, learning_rate=0.2):
     """Solve the TSP using a Self-Organizing Map."""
 
     # Obtain the normalized set of cities (w/ coord in [0,1])
@@ -20,19 +20,32 @@ def som(problem, iterations):
     # Generate an adequate population of neurons: TODO! add n as parameter
     population = generate_network(cities.shape[0] * 6)
 
+    print(population)
+
     for i in range(iterations):
-        city = cities.sample(1)
+        city = cities.sample(1)[['x', 'y']].values
         winner_idx = select_winner(population, city)
-        print(i, city)
-        print(population[winner_idx], '\n')
+        # print(population[winner_idx])
+        neighbourhood = get_neighbourhood(winner_idx, 3, population.shape[0])
+        # print(neighbourhood)
+        population[neighbourhood] += learning_rate * (city - population[neighbourhood])
+        # print(population[winner_idx])
+
+    print(population)
 
 def select_winner(population, city):
-    """Return the index of the closest neuron to a given city"""
-    return euclidean_distance(population, city[['x', 'y']].values).argmin()
+    """Return the index of the closest neuron to a given city."""
+    return euclidean_distance(population, city).argmin()
 
 def euclidean_distance(a, b):
-    """Return the array of distances of two numpy arrays of points"""
+    """Return the array of distances of two numpy arrays of points."""
     return np.linalg.norm(a - b, axis=1)
+
+def get_neighbourhood(center, radix, domain):
+    """Get the range neighbourhood of given radix around a center index."""
+    neighbourhood = np.arange(center - radix, center + radix + 1)
+    return np.mod(neighbourhood, domain)
+
 
 if __name__ == '__main__':
     main()
