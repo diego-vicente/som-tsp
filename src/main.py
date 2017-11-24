9b55564cf4f42cdc9194257c43a0f0c7632f39b3
Diego Vicente <diegovicente@protonmail.com>
2017-11-24 21:33:52 +0100

    Refactored main, meaningful output

--- src/main.py
+++ src/main.py
@@ -21,30 +21,32 @@ def som(problem, iterations, learning_rate=0.7):
     # TODO! add n as parameter
     n = cities.shape[0] * 8
 
-    # Generate an adequate population of neurons:
-    population = generate_network(n)
+    # Generate an adequate network of neurons:
+    network = generate_network(n)
+    print('Network of {} neurons created. Starting the iterations:'.format(n))
 
-    plot_tsp(cities, population, 'diagrams/before.png')
+    plot_tsp(cities, network, 'diagrams/before.png')
 
     for i in range(iterations):
+        if not i % 100:
+            print('\t> Iteration {}/{}'.format(i, iterations), end="\r")
         # Choose a random city
         city = cities.sample(1)[['x', 'y']].values
-        winner_idx = select_winner(population, city)
-        # Generate a filter that applies changes to the winner's neighbourhood
-        neighbourhood = get_neighbourhood(winner_idx, n//10, population.shape[0])
+        winner_idx = select_winner(network, city)
+        # Generate a filter that applies changes to the winner's gaussian
+        gaussian = get_neighborhood(winner_idx, n//10, network.shape[0])
         # Update the network's weights (closer to the city)
-        population += neighbourhood[:,np.newaxis] * learning_rate * (city - population)
+        network += gaussian[:,np.newaxis] * learning_rate * (city - network)
         # Decay the variables
         learning_rate = learning_rate * 0.9999
         n = n * 0.999
         if not i % 500:
-            plot_tsp(cities, population, 'diagrams/{}.png'.format(i))
+            plot_tsp(cities, network, 'diagrams/{}.png'.format(i))
         if n < 1:
             print('Radius has completely decayed, finishing execution at {} iterations'.format(i))
             break
 
-    plot_tsp(cities, population, 'diagrams/after.png')
-
+    plot_tsp(cities, network, 'diagrams/after.png')
 
 if __name__ == '__main__':
     main()
