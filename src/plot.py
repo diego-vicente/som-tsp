59fac45d45a17fe98857fa1ab5ff326250bff1ca
Diego Vicente <diegovicente@protonmail.com>
2018-01-14 13:39:21 +0100

    Modified plot functions for composed graphics

--- src/plot.py
+++ src/plot.py
@@ -1,32 +1,50 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib as mpl
 
-def plot_network(cities, neurons, name):
+def plot_network(cities, neurons, name='diagram.png', ax=None):
     """Plot a graphical representation of the problem"""
-    fig = plt.figure(figsize=(5, 5), frameon = False)
-    axis = fig.add_axes([0,0,1,1])
+    mpl.rcParams['agg.path.chunksize'] = 10000
 
-    axis.set_aspect('equal', adjustable='datalim')
-    plt.axis('off')
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon = False)
+        axis = fig.add_axes([0,0,1,1])
 
-    axis.scatter(cities['x'], cities['y'], color='red', s=4)
-    axis.plot(neurons[:,0], neurons[:,1], 'r.', ls='-', color='#0063ba', markersize=2)
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
 
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
-    plt.close()
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        axis.plot(neurons[:,0], neurons[:,1], 'r.', ls='-', color='#0063ba', markersize=2)
 
-def plot_route(cities, route, name):
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        ax.plot(neurons[:,0], neurons[:,1], 'r.', ls='-', color='#0063ba', markersize=2)
+        return ax
+
+def plot_route(cities, route, name='diagram.png', ax=None):
     """Plot a graphical representation of the route obtained"""
-    fig = plt.figure(figsize=(5, 5), frameon = False)
-    axis = fig.add_axes([0,0,1,1])
+    mpl.rcParams['agg.path.chunksize'] = 10000
 
-    axis.set_aspect('equal', adjustable='datalim')
-    plt.axis('off')
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon = False)
+        axis = fig.add_axes([0,0,1,1])
 
-    axis.scatter(cities['x'], cities['y'], color='red', s=4)
-    route = cities.reindex(route)
-    route.loc[route.shape[0]] = route.iloc[0]
-    axis.plot(route['x'], route['y'], color='purple', linewidth=1)
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
 
-    plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
-    plt.close()
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        axis.plot(route['x'], route['y'], color='purple', linewidth=1)
+
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        ax.plot(route['x'], route['y'], color='purple', linewidth=1)
+        return ax
